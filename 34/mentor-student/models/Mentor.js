8b42d1505d144e14af7439c41796f0a63a9ea175
Vetri2 <107339279+Vetri2@users.noreply.github.com>
2023-06-06 19:30:52 +0530

    Delete 34/mentor-student directory

--- 34/mentor-student/models/Mentor.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const mentorSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Mentor", mentorSchema);
--- 34/mentor-student/models/Student.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const studentSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    mentor: { type: mongoose.Schema.Types.ObjectId, ref: "Mentor" },
-});
-
-module.exports = mongoose.model("Student", studentSchema);
