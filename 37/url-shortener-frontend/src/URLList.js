1903d9c82ce45496001b1c9a6efef377e5b9c566
Vetri2 <107339279+Vetri2@users.noreply.github.com>
2023-06-18 00:06:13 +0530

    Add files via upload

---
+++ 37/url-shortener-frontend/src/URLList.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const URLList = () => {
+    const [urls, setURLs] = useState([]);
+
+    useEffect(() => {
+        const fetchURLs = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:3001/api/urls"
+                );
+                setURLs(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchURLs();
+    }, []);
+
+    return (
+        <div>
+            <h2>URL List</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Original URL</th>
+                        <th>Short URL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {urls.map((url) => (
+                        <tr key={url.id}>
+                            <td>{url.originalURL}</td>
+                            <td>{url.shortURL}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default URLList;
